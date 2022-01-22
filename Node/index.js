33413c0a14c82410d5791da2ffd4d5f87edaa52e
문규찬 <lazychan90@gmail.com>
2022-01-23 03:12:06 +0900

    post 요청시 User db 저장 가능한지 확인

--- Node/index.js
+++ Node/index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = 4000;
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json
+app.use(bodyParser.json());
 
 mongoose
   .connect(
@@ -22,6 +29,18 @@ mongoose
   });
 
 app.get("/", (req, res) => res.send("4000번에 서버 실행중!!"));
+
+app.post("/register", (req, res) => {
+  //회원가입에 필요한 정보들을 클라이언트에서 받아
+  //DB에 저장한다.
+  const user = new User(req.body);
+
+  // 몽고디비에서 가져온느 메소드
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
 
 app.listen(port, () => {
   console.log(`App listening port on ${port}`);
