04ad76a911ee093dbe6061e70f3ca6c97025908d
문규찬 <lazychan90@gmail.com>
2022-01-23 15:04:19 +0900

    bcrypt 사용해서 password 암호화 해주기 / dev.js 는 ignore 실패...

--- Node/config/dev.js
+++ Node/config/dev.js
@@ -2,3 +2,5 @@ module.exports = {
   mongoURI:
     "mongodb+srv://kyuchan:12341234@nodelogin.oqkzx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 };
+
+//dev.js 는 깃 이그노어 시켜야 하는데...
--- Node/models/User.js
+++ Node/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -31,6 +33,22 @@ const userSchema = mongoose.Schema({
     type: Number,
     // 토큰의 유효기간
   },
+});
+
+// 스키마 save 전에 Pre 단계에서 비밀번호를 암호화 해준다.
+userSchema.pre("save", function (next) {
+  var user = this;
+
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  }
 });
 
 const User = mongoose.model("User", userSchema);
