ba16639e42027465c30f99d9dd0f657c299f2922
문규찬 <lazychan90@gmail.com>
2022-01-25 14:29:33 +0900

    redux 설치후 APP과 연결

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "antd/dist/antd.css";
+
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import reduxPromise from "redux-promise";
+import reduxThunk from "redux-thunk";
+import rootRuducers from "./reducers";
+
+const createStoreWidthMiddleware = applyMiddleware(
+  reduxPromise,
+  reduxThunk
+)(createStore);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider
+      store={createStoreWidthMiddleware(
+        rootRuducers,
+        //개발자도구 리덕스 사용가능
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+    >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ client/src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+// import user from "./user_reducer"
+
+//이곳에선 결국 모든 리듀서들을 하나로 모아준다 ROOT!!!
+const rootReducers = combineReducers({
+  // user,
+});
+
+export default rootReducers;
